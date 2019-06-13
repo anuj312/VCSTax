a56a456c642d27d125e79c46ffb7861ed793cedc
UX017079 <UX017079@SP2BVDIW7DV0460.ten.thomsonreuters.com>
2019-06-13 14:27:58 +0530

    first

---
+++ VCTax/src/test/java/demo/VCTax/demo.java
@@ -0,0 +1,24 @@
+package demo.VCTax;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	
+	@Test
+	public void reports()
+	{
+		System.out.println("HELLO");
+		//*[@id="rso"]/div[1]/div/div[1]/div/div[1]/div[2]/div[3]/div/div/div[1]/a/h3
+		
+		//*[@id="logo"]/img
+		
+		//*[@id="O365_MainLink_NavMenu"]/span
+	}
+		
+		
+		
+
+	
+
+}
